36c2bc4e289b85236ce682860222c02d4c4de2e3
Tomasz Damian <Tom.Sid@wp.pl>
2017-10-17 10:08:57 +0000

    customisable module

--- Timaverkefni/verk6-7.js
+++ Timaverkefni/verk6-7.js
@@ -28,6 +28,33 @@ Vue.component("modalbox",{
 			}
 		}
 });
+Vue.component("editedmodalbox",{
+	data: function() {
+		return{
+			isActive: false
+		}
+	},
+	template:`
+	<div>
+		<button @click="OpenModal" class="button is-primary is-large modal-button">Activate modal</button>
+			<div class="modal" :class="isActive?'is-active':''">
+				<div @click="CloseModal" class="modal-background"></div>
+					<div class="modal-content">
+						<slot></slot>
+					</div>
+				<button @click="CloseModal" class="modal-close is-large" aria-label="close"></button>
+			</div>
+	</div>
+		`,
+		methods:{
+			CloseModal: function(){
+				this.isActive = false;
+			},
+			OpenModal: function(){
+				this.isActive = true
+			}
+		}
+});
 Vue.component("card",{
 	props:["title","subtitle","image","avatar"],
 	template:
